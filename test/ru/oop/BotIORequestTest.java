543b6ce7a7f0db0c8a8aaf3aab1aea8aa43d053f
complito <hardway186@gmail.com>
2021-10-12 18:44:00 +0500

    Исправлены ошибки в BotIORequestTest

--- test/ru/oop/BotIORequestTest.java
+++ test/ru/oop/BotIORequestTest.java
@@ -8,27 +8,26 @@ class BotIORequestTest {
 
     @Test
     void testEmptyRequest() {
-        assertEquals(botIO.Request(""), new String[] {"Ошибка: запрос пустой"});
+        assertEquals(botIO.Request("")[0], "Ошибка: запрос пустой");
     }
 
     @Test
     void testHelpMessage() {
-        assertEquals(botIO.Request("\\help"), new String[] {"Привет, я бот который умеет находить песню по отрывку ее текста. Если нужна помощь, напиши \\help"});
+        assertEquals(botIO.Request("\\help")[0], "\\findsong - Поиск песни по отрывку текста");
     }
 
     @Test
     void testSongsNotFound() {
-        assertEquals(botIO.Request("\\findsong dasddasdwq"), new String[] {"По введённому запросу не было найдено песен"});
+        assertEquals(botIO.Request("\\findsong dasddasdwq")[0], "По введённому запросу не было найдено песен");
     }
 
     @Test
     void testSongsFound() {
-        String[] botRequest = botIO.Request("\\findsong Humble");
-        assertEquals(botRequest[0], "Список найденных песен:");
+        assertEquals(botIO.Request("\\findsong Humble")[0], "Список найденных песен:");
     }
 
     @Test
     void testUnknownRequest() {
-        assertEquals(botIO.Request("sda"), new String[] {"Ошибка: неизвестный запрос"});
+        assertEquals(botIO.Request("sda")[0], "Ошибка: неизвестный запрос");
     }
 }
