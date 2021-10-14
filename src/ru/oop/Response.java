42afc0cbfd953c20b91c9934b3a66f54433bd652
lilangel <cvndyperov@gmail.com>
2021-10-14 10:16:35 +0500

    edited readme

--- src/ru/oop/Response.java
+++ src/ru/oop/Response.java
@@ -6,12 +6,15 @@ import java.util.List;
 public class Response {
     public String responseString;
     public List<Song> responseList;
-    Response(String responseString){
+
+    public Response(String responseString){
         this.responseString = responseString;
     }
-    Response(List<Song> responseList){
+
+    public Response(List<Song> responseList){
         this.responseList = responseList;
     }
+
     public void SongListToStr() {
         responseString = "";
         responseString += "Список найденных песен:\n";
